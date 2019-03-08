b6901360277efb9fbfb6c6c81594d3f4eaba7983
tehryn <thetehryn@gmail.com>
2019-03-08 10:20:25 +0100

    oprava drobnych chyb + komentare

--- proj1/bks.cpp
+++ proj1/bks.cpp
@@ -1,10 +1,171 @@
+/**
+ * Author: Jiri Matejka
+ */
 #include "bks.h"
-const int BAD_ALLOCK = 1;
-const int BAD_PROC_COUNT = 2;
 
-const int NUMBER_TAG = 1;
+int main( int argc, char **argv ) {
+    int procCount, procId, treeLevel, myLevel;;
+    MPI::Init();
+    
+    // number of processes
+    MPI_Comm_size( MPI_COMM_WORLD, &procCount );
+    
+    // id of current process
+    procId = MPI::COMM_WORLD.Get_rank();
+    
+    // number of list processes
+    size_t listCount = ( procCount + 1 ) / 2;
+    
+    // size of file
+    unsigned long long fileSize;
+    
+    // Only root process will read from file
+    if ( procId == 0 ) {
+        if ( argc == 1 ) {
+            fileSize = getFileSize( argv[1] );
+        }
+        else {
+            fileSize = getFileSize( "numbers" );
+        }
+        int expected = expectedProcCount( fileSize, procCount );
+        if ( expected != procCount ) {
+            std::cerr << "Invalid number of processes, expected: " << expected << ", got " << procCount << std::endl; 
+            MPI_Abort(MPI_COMM_WORLD, BAD_PROC_COUNT);
+        }
+    }
+    
+    // broadcast filesize to other processes
+    MPI_Bcast(&fileSize, 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
+    
+    if ( fileSize == 0 ) {
+        if ( procId == 0 ) {
+            std::cerr << "Error while reading from file: Unable to open file or file is empty." << std::endl; 
+        }
+        MPI_Abort(MPI_COMM_WORLD, FILE_ERROR);
+    }
+    
+    // test if current process is list process
+    bool listProc = procId >= static_cast<int>( procCount - listCount );
+    
+    // bucket size for list processes
+    size_t baseBucketSize = ( fileSize % listCount == 0 ) ? ( fileSize / listCount ) : ( ( fileSize / listCount ) + 1 );
+    
+    // bucket size for current process
+    size_t bucketSize = baseBucketSize; 
+    
+    // list processes does not care about their level in tree
+    // also their bucketSize was already set
+    if ( !listProc ) {
+        treeLevel  = log2( listCount ) + 1;
+        myLevel    = log2( procId + 1 );
+        bucketSize = pow( 2, treeLevel - myLevel - 1) * baseBucketSize;
+    }
+    
+    // allocating data for bucket
+    unsigned char * bucketData = nullptr;
+    try { 
+        bucketData = new unsigned char[ bucketSize ];
+    } catch ( std::bad_alloc & ba ) { 
+        std::cerr << "Unable to allocate memory for bucket: " << ba.what(); 
+        MPI_Abort(MPI_COMM_WORLD, BAD_ALLOCK);
+    }
 
-#include <unistd.h> // TODO
+    if ( procId == 0 ) {
+        
+        // loads input to bucket
+        if ( !readBytes( bucketData, fileSize, argv[1] ) ) {
+            std::cerr << "Error while reading from file: Was not able to read whole file content." << std::endl; 
+            MPI_Abort(MPI_COMM_WORLD, FILE_ERROR);
+        }
+        
+        // adds extra 255 so each bucket will have same size
+        for ( size_t i = fileSize; i < bucketSize; i++ ) {
+            bucketData[ i ] = 255;
+        }
+        
+        // Printing read sequence to stdout
+        for ( size_t i = 0; i < fileSize; i++ ) {
+            std::cout << +bucketData[ i ] << ((i + 1 == fileSize) ? "" : " ");
+        }
+        std::cout << std::endl;
+        
+        /* TODO - delete */
+        double t1, t2; 
+        t1 = MPI_Wtime();
+        
+        // sending data to list processes
+        size_t dest = procCount - 1;
+        size_t start = 0;
+        for( size_t j = 0; j < listCount; j++ ) {
+            MPI_Send( bucketData + start, baseBucketSize, MPI_UNSIGNED_CHAR, dest-- , NUMBER_TAG, MPI_COMM_WORLD);
+            start += baseBucketSize;
+        }
+        
+        // reading data from node processes
+        size_t readSize = bucketSize >> 1;
+        MPI_Recv( bucketData, readSize, MPI_UNSIGNED_CHAR, 1, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+        MPI_Recv( bucketData + readSize, readSize, MPI_UNSIGNED_CHAR, 2, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+        
+        // allocating memory for sorted array
+        unsigned char * sorted = nullptr;
+        try { 
+            sorted = new unsigned char[ bucketSize ];
+        } catch ( std::bad_alloc & ba ) { 
+            std::cerr << "Unable to allocate memory for bucket: " << ba.what(); 
+            MPI_Abort(MPI_COMM_WORLD, BAD_ALLOCK);
+        }
+        
+        // merging sorted arrays
+        merge(bucketData, bucketData + readSize, readSize, sorted);
+        
+        /* TODO - delete */
+        t2 = MPI_Wtime();
+        std::cerr << fileSize << ";" << t2 -t1 << std::endl;
+        
+        // printing result to stdout
+        for ( size_t i = 0; i < fileSize; i++ ) {
+            std::cout << +sorted[ i ] << std::endl;
+        }
+        delete [] sorted;
+    }
+    else {
+        // destination where sorted data will be sent
+        int merger = ( procId % 2 ) == 0 ? procId / 2 -1 : procId / 2;
+        if ( listProc ) {
+            MPI_Recv( bucketData, bucketSize, MPI_UNSIGNED_CHAR, 0, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+            shellSort( bucketData, bucketSize );
+            MPI_Send( bucketData, bucketSize, MPI_UNSIGNED_CHAR, merger, NUMBER_TAG, MPI_COMM_WORLD);
+        }
+        else {
+            // from who I will read data
+            int recvFrom1  = ( procId << 1 ) + 1;
+            int recvFrom2  = recvFrom1 + 1;
+            
+            // size o array where input data will be merged
+            size_t readSize = bucketSize >> 1;
+            MPI_Recv( bucketData, readSize, MPI_UNSIGNED_CHAR, recvFrom1, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+            MPI_Recv( bucketData + readSize, readSize, MPI_UNSIGNED_CHAR, recvFrom2, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+            
+            // allocating memory for array
+            unsigned char * sorted = nullptr;
+            try { 
+                sorted = new unsigned char[ bucketSize ];
+            } catch ( std::bad_alloc & ba ) { 
+                std::cerr << "Unable to allocate memory for bucket: " << ba.what(); 
+                MPI_Abort(MPI_COMM_WORLD, BAD_ALLOCK);
+            }
+            
+            // merging 2 sorted array to new allocated array
+            merge(bucketData, bucketData + readSize, readSize, sorted);
+            
+            MPI_Send( sorted, bucketSize, MPI_UNSIGNED_CHAR, merger, NUMBER_TAG, MPI_COMM_WORLD);
+            delete [] sorted;
+        }
+    }
+    delete [] bucketData;
+    MPI::Finalize();
+    return 0;
+}
 
 size_t getFileSize( const char * filename ) {
     std::streampos fsize = 0;
@@ -18,10 +179,11 @@ size_t getFileSize( const char * filename ) {
     return fsize;
 }
 
-void readBytes( unsigned char * array, size_t size, const char * filename ) {
+bool readBytes( unsigned char * array, size_t size, const char * filename ) {
+    size_t i = 0;
     std::ifstream file( filename, std::ios::binary );
     if ( file.is_open() ) {
-        for( size_t i = 0; i < size; i++ ) {
+        for(; i < size; i++ ) {
             if ( file.good() ) {
                 array[ i ] = file.get();
             }
@@ -31,6 +193,7 @@ void readBytes( unsigned char * array, size_t size, const char * filename ) {
         }
         file.close();
     }
+    return i == size;
 }
 
 void merge( unsigned char * arr1, unsigned char * arr2, size_t size, unsigned char * result ) {
@@ -78,118 +241,4 @@ int expectedProcCount( size_t fileSize, int procCount ) {
         }
     }
     return 2 * nextPower - 1;
-}
-
-int main( int argc, char **argv ) {
-    int procCount, nameSize, procId, treeLevel, myLevel;;
-    char procName[ MPI_MAX_PROCESSOR_NAME ];
-    MPI::Init();
-    MPI_Comm_size( MPI_COMM_WORLD, &procCount );
-    MPI_Get_processor_name( procName, &nameSize );
-    procId = MPI::COMM_WORLD.Get_rank();
-    
-    size_t listCount = ( procCount + 1 ) / 2;
-    size_t fileSize;
-    if ( procId == 0 ) {
-        fileSize = getFileSize( argv[1] );
-        int expected = expectedProcCount( fileSize, procCount );
-        if ( expected != procCount ) {
-            std::cerr << "Invalid number of processes, expected: " << expected << ", got " << procCount << std::endl; 
-            MPI_Abort(MPI_COMM_WORLD, BAD_PROC_COUNT);
-        }
-    }
-    MPI_Bcast(&fileSize, sizeof( fileSize ), MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
-    
-    if ( fileSize == 0 ) {
-        MPI::Finalize();
-        return 0;
-    }
-    
-    size_t baseBucketSize = ( fileSize % listCount == 0 ) ? ( fileSize / listCount ) : ( ( fileSize / listCount ) + 1 );
-    size_t bucketSize = baseBucketSize; 
-    bool listProc = procId >= static_cast<int>( procCount - listCount );
-    if ( !listProc ) {
-        treeLevel  = log2( listCount ) + 1;
-        myLevel    = log2( procId + 1 );
-        bucketSize = pow( 2, treeLevel - myLevel - 1) * baseBucketSize;
-    }
-    
-    unsigned char * bucketData = nullptr;
-    try { 
-        bucketData = new unsigned char[ bucketSize ];
-    } catch ( std::bad_alloc & ba ) { 
-        std::cerr << "Unable to allocate memory for bucket: " << ba.what(); 
-        MPI_Abort(MPI_COMM_WORLD, BAD_ALLOCK);
-    }
-
-    if ( procId == 0 ) {
-        readBytes( bucketData, fileSize, argv[1] );
-        for ( size_t i = fileSize; i < bucketSize; i++ ) {
-            bucketData[ i ] = 255;
-        }
-        for ( size_t i = 0; i < fileSize; i++ ) {
-            std::cout << +bucketData[ i ] << ((i + 1 == fileSize) ? "" : " ");
-        }
-        
-        std::cout << std::endl;
-        
-        double t1, t2; 
-        t1 = MPI_Wtime();
-        
-        size_t dest = procCount - 1;
-        size_t start = 0;
-        for( size_t j = 0; j < listCount; j++ ) {
-            MPI_Send( bucketData + start, baseBucketSize, MPI_UNSIGNED_CHAR, dest-- , NUMBER_TAG, MPI_COMM_WORLD);
-            start += baseBucketSize;
-        }
-        
-        size_t readSize = bucketSize >> 1;
-        MPI_Recv( bucketData, readSize, MPI_UNSIGNED_CHAR, 1, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-        MPI_Recv( bucketData + readSize, readSize, MPI_UNSIGNED_CHAR, 2, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-        unsigned char * sorted = nullptr;
-        try { 
-            sorted = new unsigned char[ bucketSize ];
-        } catch ( std::bad_alloc & ba ) { 
-            std::cerr << "Unable to allocate memory for bucket: " << ba.what(); 
-            MPI_Abort(MPI_COMM_WORLD, BAD_ALLOCK);
-        }
-        
-        merge(bucketData, bucketData + readSize, readSize, sorted);
-        
-        t2 = MPI_Wtime();
-        std::cerr << fileSize << ";" << t2 -t1 << std::endl;
-        for ( size_t i = 0; i < fileSize; i++ ) {
-            std::cout << +sorted[ i ] << std::endl;
-        }
-        delete [] sorted;
-    }
-    else {
-        int merger = ( procId % 2 ) == 0 ? procId / 2 -1 : procId / 2;
-        if ( listProc ) {
-            MPI_Recv( bucketData, bucketSize, MPI_UNSIGNED_CHAR, 0, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-            shellSort( bucketData, bucketSize );
-            MPI_Send( bucketData, bucketSize, MPI_UNSIGNED_CHAR, merger, NUMBER_TAG, MPI_COMM_WORLD);
-        }
-        else {
-            int recvFrom1  = procId * 2 + 1;
-            int recvFrom2  = recvFrom1 + 1;
-            size_t readSize = bucketSize >> 1;
-            MPI_Recv( bucketData, readSize, MPI_UNSIGNED_CHAR, recvFrom1, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-            MPI_Recv( bucketData + readSize, readSize, MPI_UNSIGNED_CHAR, recvFrom2, NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-            
-            unsigned char * sorted = nullptr;
-            try { 
-                sorted = new unsigned char[ bucketSize ];
-            } catch ( std::bad_alloc & ba ) { 
-                std::cerr << "Unable to allocate memory for bucket: " << ba.what(); 
-                MPI_Abort(MPI_COMM_WORLD, BAD_ALLOCK);
-            }
-            merge(bucketData, bucketData + readSize, readSize, sorted);
-            MPI_Send( sorted, bucketSize, MPI_UNSIGNED_CHAR, merger, NUMBER_TAG, MPI_COMM_WORLD);
-            delete [] sorted;
-        }
-    }
-    delete [] bucketData;
-    MPI::Finalize();
-    return 0;
 }
--- proj1/bks.h
+++ proj1/bks.h
@@ -1,9 +1,51 @@
+/**
+ * Author: Jiri Matejka
+ */
 #include <iostream>
 #include <fstream>
 #include <mpi.h>
 #include <sstream>
 #include <algorithm>
 #include <math.h>
+
+const int BAD_ALLOCK = 1;
+const int BAD_PROC_COUNT = 2;
+const int FILE_ERROR = 3;
+
+const int NUMBER_TAG = 1;
+
+/**
+ * Finds out size of a file.
+ * @param  filename name of file
+ * @return size of file. If size is 0 or its unable to open file, returns 0.
+ */
+size_t getFileSize( const char * filename );
+
+/**
+ * Reads file content into array.
+ * @param array    array with allocated memory for content
+ * @param size     size of bytes to be read
+ * @param filename name of file
+ * @return true, if number of bytes read is equal to size. Otherwise false.
+ */
+bool readBytes( unsigned char * array, size_t size, const char * filename );
+
+/**
+ * Merges 2 sorted arrays into one sorted array
+ * @param arr1   1st array to be merged
+ * @param arr2   2nd array to be merged
+ * @param size   size of 1st and 2nd array
+ * @param result array with allocated memory where result will be stored
+ */
+void merge( unsigned char * arr1, unsigned char * arr2, size_t size, unsigned char * result );
+
+/**
+ * Sequence sort algorithm.
+ * @param array array that will be sorted
+ * @param size  size of array
+ */
+void shellSort(unsigned char * array, size_t size);
+int expectedProcCount( size_t fileSize, int procCount );
 
 template<typename T1>
 void DEBUG_INLINE(T1 x) {
